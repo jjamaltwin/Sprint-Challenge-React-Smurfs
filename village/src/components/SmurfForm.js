3271978390a0ec011971c702a6cd409798fa4efe
unknown <jjamal95@gmail.com>
2019-06-16 22:05:47 -0500

    finished Get request, and Post functionality, also have both rendering

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from "axios";
+
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -12,12 +14,20 @@ class SmurfForm extends Component {
 
   addSmurf = event => {
     event.preventDefault();
-    // add code to create the smurf using the api
+    event.preventDefault();
+    const newbSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    };
 
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
+    axios.post("http://localhost:3333/smurfs", newbSmurf)
+    .then(newbSmurf => {
+      this.setState({ name: "", age: "", height: "" });
+      this.props.history.push( )
+    })
+    .catch(error => {
+      console.log(error);
     });
   }
 
